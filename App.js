4be01680cd8428e02cec84e27500418b9b0774af
JayoonKoo <rnwkdbs12@gmail.com>
2021-12-15 17:17:44 +0900

    feat: main template

--- App.js
+++ App.js
@@ -5,29 +5,15 @@
  * @format
  */
 
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import Counter from './components/Counter';
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const onIncrease = () => {
-    setCount(count + 1);
-  };
-  const onDecrease = () => {
-    setCount(count - 1);
-  };
   return (
-    <SafeAreaView style={styles.full}>
-      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
+    <SafeAreaView>
+      <Text>Main</Text>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  full: {
-    flex: 1,
-  },
-});
 
 export default App;
